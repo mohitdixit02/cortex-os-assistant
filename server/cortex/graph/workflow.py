aa959d8203b754c59b1f3ed176da7b17579e454c
Mohit Sharma <mohit.vsht@gmail.com>
2026-04-15 23:57:21 +0530

    feature: integration -  main x manager

--- server/cortex/graph/workflow.py
+++ server/cortex/graph/workflow.py
@@ -13,42 +13,70 @@ orchestrator = Orchestrator()
 main_graph = StateGraph(ConversationState)
 
 main_graph.add_node("fetch_stm", memory_client.fetch_relevant_stm)
-# main_graph.add_node("fetch_emotional_profile", memory_client.fetch_emotional_profile)
-# main_graph.add_node("plan_main_orchestration", orchestrator.build_main_orchestration_plan)
-# main_graph.add_node("fetch_user_knowledge_base", memory_client.fetch_relevant_knowledge_base)
-# main_graph.add_node("fetch_message_history", memory_client.fetch_relevant_message_history)
-# main_graph.add_node("plan_evaluation", orchestrator.evaluate_plan)
-# main_graph.add_node("final_response_generation", orchestrator.generate_final_response)
-# main_graph.add_node("final_response_alignment", orchestrator.align_final_response)
-# main_graph.add_node("execute_tools_manager", manager_client.execute_tools)
-# main_graph.add_node("summarize_tool_result", manager_client.summarize_tool_results)
+main_graph.add_node("fetch_emotional_profile", memory_client.fetch_emotional_profile)
+main_graph.add_node("main_orchestration", orchestrator.main_orchestration)
+main_graph.add_node("build_knowledge_plan", orchestrator.build_knowledge_plan)
+main_graph.add_node("build_messages_plan", orchestrator.build_messages_plan)
+main_graph.add_node("build_tools_plan", orchestrator.build_tools_plan)
+main_graph.add_node("evaluate_knowledge_plan", orchestrator.evaluate_knowledge_plan)
+main_graph.add_node("evaluate_messages_plan", orchestrator.evaluate_messages_plan)
+main_graph.add_node("evaluate_tools_plan", orchestrator.evaluate_tools_plan)
+main_graph.add_node("evaluation_aggregator", orchestrator.evaluation_aggregator)
+main_graph.add_node("fetch_user_knowledge_base", memory_client.fetch_relevant_knowledge_base)
+main_graph.add_node("fetch_message_history", memory_client.fetch_relevant_message_history)
+main_graph.add_node("execute_tools_manager", orchestrator.execute_tools)
+main_graph.add_node("final_response_generation", orchestrator.generate_final_response)
+main_graph.add_node("final_response_alignment", orchestrator.align_final_response)
 
 main_graph.add_edge(START, "fetch_stm")
-# main_graph.add_edge(START, "fetch_emotional_profile")
-# main_graph.add_edge("fetch_stm", "plan_main_orchestration")
-# main_graph.add_edge("fetch_emotional_profile", "plan_main_orchestration")
-# main_graph.add_edge("plan_main_orchestration", "fetch_user_knowledge_base")
-# main_graph.add_edge("plan_main_orchestration", "fetch_message_history")
-# main_graph.add_edge("plan_main_orchestration", "execute_tools_manager")
-# main_graph.add_edge("execute_tools_manager", "summarize_tool_result")
-# main_graph.add_edge(["fetch_user_knowledge_base", "fetch_message_history", "summarize_tool_result"], "plan_evaluation")
-# main_graph.add_conditional_edges(
-#     "plan_evaluation", 
-#     orchestrator.route_condition_orchestration_evaluation,
-#     {
-#         "plan_main_orchestration": "plan_main_orchestration",
-#         "final_response_generation": "final_response_generation",
-#     },
-# )
-# main_graph.add_edge("final_response_generation", "final_response_alignment")
-# main_graph.add_conditional_edges(
-#     "final_response_alignment",
-#     orchestrator.route_condition_final_response_evaluation,
-#     {
-#         "final_response_generation": "final_response_generation",
-#         "terminate": END,
-#     },
-# )
+main_graph.add_edge(START, "fetch_emotional_profile")
+main_graph.add_edge(["fetch_stm", "fetch_emotional_profile"], "main_orchestration")
+main_graph.add_conditional_edges(
+    "main_orchestration",
+    orchestrator.route_main_orchestration,
+    {
+        "build_knowledge_plan": "build_knowledge_plan",
+        "build_messages_plan": "build_messages_plan",
+        "build_tools_plan": "build_tools_plan",
+        "final_response_generation": "execute_tools_manager",
+    },
+)
+
+main_graph.add_edge("build_knowledge_plan", "fetch_user_knowledge_base")
+main_graph.add_edge("fetch_user_knowledge_base", "evaluate_knowledge_plan")
+main_graph.add_conditional_edges(
+    "build_messages_plan",
+    orchestrator.route_condition_fetch_messages,
+    {
+        "fetch_message_history": "fetch_message_history",
+        "skip_message_retrieval": "evaluate_messages_plan",
+    }
+)
+main_graph.add_edge("fetch_message_history", "evaluate_messages_plan")
+main_graph.add_edge("build_tools_plan", "evaluate_tools_plan")
+main_graph.add_edge(
+    ["evaluate_knowledge_plan", "evaluate_messages_plan", "evaluate_tools_plan"],
+    "evaluation_aggregator",
+)
+
+main_graph.add_conditional_edges(
+    "evaluation_aggregator",
+    orchestrator.route_condition_orchestration_evaluation,
+    {
+        "plan_main_orchestration": "main_orchestration",
+        "final_response_generation": "execute_tools_manager",
+    },
+)
+main_graph.add_edge("execute_tools_manager", "final_response_generation")
+main_graph.add_edge("final_response_generation", "final_response_alignment")
+main_graph.add_conditional_edges(
+    "final_response_alignment",
+    orchestrator.route_condition_final_response_evaluation,
+    {
+        "final_response_generation": "final_response_generation",
+        "terminate": END,
+    },
+)
 
 main_workflow = main_graph.compile()
 
@@ -58,7 +86,7 @@ def display_workflow_graph(worflow):
     img = Image.open(io.BytesIO(image_data))
     img.show()
 
-# display_workflow_graph(main_workflow)
+display_workflow_graph(main_workflow)
 # display_workflow_graph(build_memory_workflow)
 
 # test workflow
